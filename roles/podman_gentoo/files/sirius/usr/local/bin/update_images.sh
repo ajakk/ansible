89b5a48baab96547252f2c5ff93267e34320d07e
John Helmert III <jchelmert3@posteo.net>
2022-08-28 13:05:15 -0500

    *: more switching to public_domain
    Signed-off-by: John Helmert III <jchelmert3@posteo.net>

--- roles/podman_gentoo/files/sirius/usr/local/bin/update_images.sh
+++ roles/podman_gentoo/files/sirius/usr/local/bin/update_images.sh
@@ -3,7 +3,7 @@
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 function update() {
-	podman-gentoo "${1}" "/etc/podman-gentoo/${1}" "/var/cache/binpkgs" registry.wg.home.arpa
+	podman-gentoo "${1}" "/etc/podman-gentoo/${1}" "/var/cache/binpkgs" registry.wg.ajak.xyz
 }
 
 update sirius
