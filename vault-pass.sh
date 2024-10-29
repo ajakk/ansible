e459312ca7831e7d5f4b8bb6679d08ce27b71b41
John Helmert III <jchelmert3@posteo.net>
2024-10-28 19:09:08 -0700

    vault-pass: accommodate gopass
    Signed-off-by: John Helmert III <jchelmert3@posteo.net>

--- vault-pass.sh
+++ vault-pass.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
-pass show Home/ansible-vault
+#!/usr/bin/env bash
+if type -P pass 2>&1 >/dev/null; then
+    pass show Home/ansible-vault
+elif type -P gopass 2>&1 >/dev/null; then
+    gopass show Home/ansible-vault
+else
+    exit 1
+fi
