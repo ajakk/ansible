09b1497558c0c42a9b5f8888d11194ff54dd6ec2
John Helmert III <jchelmert3@posteo.net>
2025-01-04 11:34:14 -0600

    vault-pass: don't write to stderr
    Signed-off-by: John Helmert III <jchelmert3@posteo.net>

--- vault-pass.sh
+++ vault-pass.sh
@@ -4,5 +4,5 @@ if type -P pass 2>&1 >/dev/null; then
 elif type -P gopass 2>&1 >/dev/null; then
     gopass show Home/ansible-vault
 else
-    echo "Couldn't get vault password! Seems like a pass client is not installed." >2
+    echo "Couldn't get vault password! Seems like a pass client is not installed."
 fi
